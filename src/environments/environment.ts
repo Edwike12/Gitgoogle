a26fe28c3c5b580f572c48a87e8a8e08548ead02
Edwike12 <nyaunchoedwike@gmail.com>
2021-12-03 13:57:09 +0300

    added an access token to environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  userUrl:"https://api.github.com/users/",
+  accessToken:"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"
 };
 
 /*
